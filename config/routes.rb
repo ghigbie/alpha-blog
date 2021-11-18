0deb1f98ed1aa4f1e1c7544567aceacc4f914691
ghigbie <gehigbie@gmail.com>
2021-11-17 20:22:36 -0500

    Edit routes and templates

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'contactus', to: 'pages#contact'
+  resources :articles, only: [:show, :index]
 end
