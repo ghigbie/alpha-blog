b7bd8af21c2a43045cc11314820237a22bc191b7
ghigbie <gehigbie@gmail.com>
2021-11-18 20:02:48 -0500

    Add form with form_with

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'contactus', to: 'pages#contact'
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
 end
